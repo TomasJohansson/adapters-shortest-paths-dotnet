c5e05f0b8192e00ce121375117295cab0e18d23c
Tomas Johansson <github-tomas@programmerare.com>
2018-06-03 20:30:29 +0200

    Added a new test project, targeting .NET Framework 4.7.2. It was generated with Visual Studio 2017.

---
+++ K-shortest-paths-Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_shortest_paths_Test
+{
+    public class Class1
+    {
+    }
+}
