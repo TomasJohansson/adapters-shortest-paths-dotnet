22b77076e33c2986ba3f1c3ab62f4f624168f72f
Tomas Johansson <github-tomas@programmerare.com>
2018-06-03 20:38:02 +0200

    Moved test files (two source code .cs files with test code and some test dara files) to the new test library. The project file (.csproj) is configured with "CopyToOutputDirectory" for the test data files.

--- K-shortest-paths-Test/Class1.cs
+++ K-shortest-paths-Test/Class1.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//using NUnit.Framework;
 namespace K_shortest_paths_Test
 {
+    //[TestFixture]
     public class Class1
     {
+    //    public void testar()
+    //    {
+    //        NUnit.Framework.Assert.AreEqual(3,4);
+    //    }
     }
 }
