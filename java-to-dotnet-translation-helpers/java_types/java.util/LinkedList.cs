02cd85af961f50a432d2c5cd5a88efd2b2ced492
Tomas Johansson <github-tomas@programmerare.com>
2018-07-02 21:10:28 +0200

    Fixed the implementation of LinkedList since the "YanQi" implementation broke at the recent previous merge with "bsmock" branch.

--- java-to-dotnet-translation-helpers/java_types/java.util/LinkedList.cs
+++ java-to-dotnet-translation-helpers/java_types/java.util/LinkedList.cs
@@ -5,106 +5,53 @@ using System.Collections.Generic;
 namespace java.util
 {
     // https://docs.oracle.com/javase/7/docs/api/java/util/LinkedList.html
-    public class LinkedList<T> : List<T>
+    public class LinkedList<T> : List<T> , IEnumerator<T> , IEnumerable<T>
     {
-        System.Collections.Generic.LinkedList<T> _linkedList = new System.Collections.Generic.LinkedList<T>();
+        // IEnumerator/IEnumerable are needed to support "foreach" iteration
 
-        public T Current => default(T);
+        private System.Collections.Generic.List<T> _list = new System.Collections.Generic.List<T>();
 
-        object IEnumerator.Current {
+        object IEnumerator.Current
+        {
             get
             {
-                return default(T);
+                return Current;
             }
         }
 
-        public T getLast()
-        {
-            return _linkedList.Last.Value;
-        }
+        private int position = -1;
 
-        public T getFirst()
+        public T Current
         {
-            // Returns the first element in this list.
-            // exception if empty
-            return _linkedList.First.Value;
-        }
-
-        public void addFirst(T t)
-        {
-            _linkedList.AddFirst(t);
-        }
-
-        public void addLast(T t)
-        {
-            _linkedList.AddLast(t);
-        }
-
-        public Iterator<T> iterator()
-        {
-            return new Iterator<T>(_linkedList);
-        }
-
-        // javadoc: 
-        // "This method is equivalent to addLast(E)."
-        // https://docs.oracle.com/javase/7/docs/api/java/util/LinkedList.html#add(E)
-        public void add(T t)
-        {
-            addLast(t);
-        }
-
-        public void add(int index, T element)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T remove(int i)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T get(int i) {
-            // java linked list has this method get(i)
-            // but .NET linked list does not have _linkedList[i]
-            // so therefore has to implement in some otehr way ...
-            // TODO something better than slow iteration below ...
-            var enumerator = this.GetEnumerator();
-            int counter = -1;
-            while(enumerator.MoveNext())
+            get
             {
-                counter++;
-                if(counter == i) break;
+                try
+                {
+                    return _list[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
             }
-            return enumerator.Current;
+        }
+
+        public void addAll(java.util.List<T> list)
+        {
+            for(int i=0; i<list.size(); i++)
+            {
+                this.add(list.get(i));
+            }
         }
 
         public void clear()
         {
-            throw new NotImplementedException();
+            _list.Clear();
         }
 
-        public void addAll(List<T> list)
+        public void add(T t)
         {
-            IEnumerator<T> enumerator = list.GetEnumerator();
-            while(enumerator.MoveNext())
-            {
-                this.add(enumerator.Current);
-            }
-        }
-
-        public int indexOf(T t)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<T> subList(int fromIndex, int toIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int size()
-        {
-            return _linkedList.Count;
+            _list.Add(t);
         }
 
         public bool isEmpty()
@@ -112,34 +59,126 @@ namespace java.util
             return size() == 0;
         }
 
+        public int size()
+        {
+            return _list.Count;
+        }
+
+        // https://docs.oracle.com/javase/7/docs/api/java/util/List.html#subList(int,%20int)
+        public List<T> subList(int fromIndex, int toIndex)
+        {
+            var newList = new LinkedList<T>();
+            for (int i = fromIndex; i < toIndex; i++)
+            {
+                newList.add(this.get(i));
+            }
+            return newList;
+        }
+
+        public int indexOf(T t)
+        {
+            return _list.IndexOf(t);
+        }
+
+        public override int GetHashCode()
+        {
+            // return base.GetHashCode();
+            // IMPORTANT: The above standard hashcode 
+            // will NOT work! Tests will fail !
+
+            // The below implementation are copied from here:
+            // https://docs.oracle.com/javase/7/docs/api/java/util/List.html#hashCode()
+            int hashCode = 1;
+            foreach (T e in this._list)
+            {
+                hashCode = 31*hashCode + (e==null ? 0 : e.GetHashCode());
+            }
+            return hashCode;
+        }
+
+        public T get(int i)
+        {
+            return _list[i];
+        }
+
+        public T remove(int i)
+        {
+            var element = get(i);
+            _list.RemoveAt(i);
+            return element;
+        }
+
+        // https://docs.oracle.com/javase/7/docs/api/java/util/List.html#add(int,%20E)
+        // Inserts the specified element at the specified position in this list.
+        // Shifts the element currently at that position (if any) and any subsequent 
+        // elements to the right (adds one to their indices).
+        public void add(int index, T element)
+        {
+            _list.Insert(index, element);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
         public bool MoveNext()
         {
-            throw new NotImplementedException();
+            // https://msdn.microsoft.com/en-us/library/system.collections.ienumerator.current(v=vs.110).aspx
+            position++;
+            return (position < _list.Count);
         }
 
         public void Reset()
         {
-            throw new NotImplementedException();
+            position = -1;
         }
 
         public void Dispose()
         {
         }
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            return _linkedList.GetEnumerator();
-        }
-
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return _linkedList.GetEnumerator();
-            //return new LinkedListEnumerator<T>(_linkedList);
+            return _list.GetEnumerator();
+        }
+
+	    public override String ToString() {
+		    return _list.ToString();
+	    }
+
+        public T getFirst()
+        {
+            return get(0);
+        }
+
+        public T getLast()
+        {
+            return get(size()-1);
+        }
+
+        public void addFirst(T t)
+        {
+            _list.Insert(0, t);
+        }
+
+        public Iterator<T> iterator()
+        {
+            return new Iterator<T>(_list);
         }
 
         public void __Reverse()
         {
-            throw new NotImplementedException();
+            _list.Reverse();
         }
+        //public IEnumerator<T> GetEnumerator()
+        //{
+        //    return _list.GetEnumerator();
+        //}
+
+        //IEnumerator IEnumerable.GetEnumerator()
+        //{
+        //    return _list.GetEnumerator();
+        //}
     }
 }
