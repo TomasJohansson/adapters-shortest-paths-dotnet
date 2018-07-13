2d648e14a1db7c418c206f3e6e70333f289fc1c2
Tomas Johansson <github-tomas@programmerare.com>
2018-07-13 21:33:41 +0200

    Renamed two projects: java-to-dotnet-translation-helpers --> K-shortest-paths-DotNet-helpers java-to-dotnet-translation-helpers-test --> K-shortest-paths-DotNet-helpers-test

--- K-shortest-paths-DotNet-helpers-test/Properties/AssemblyInfo.cs
+++ K-shortest-paths-DotNet-helpers-test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("java-to-dotnet-translation-helpers-test")]
+[assembly: AssemblyTitle("K-shortest-paths-DotNet-helpers-test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("java-to-dotnet-translation-helpers-test")]
+[assembly: AssemblyProduct("K-shortest-paths-DotNet-helpers-test")]
 [assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
