1243ab574c03077c6e5e58d58da8594f63af8ec2
Tomas Johansson <github-tomas@programmerare.com>
2018-07-15 00:37:04 +0200

    Some code for using a SQLite file in a subdirectory named "sqlite_directory" rather than using the subdirectory "bin/Debug" as the location of the SQLite file.

--- dotnet-adapters-shortest-paths-example-project/main/roadrouting/database/PersistenceSessionFactory.cs
+++ dotnet-adapters-shortest-paths-example-project/main/roadrouting/database/PersistenceSessionFactory.cs
@@ -12,7 +12,7 @@ namespace roadrouting.database {
           return Fluently.Configure()
             .Database(
               SQLiteConfiguration.Standard
-                .UsingFile(DbFile)
+                .UsingFile(GetFullPathToSQLiteFile())
             )
             .Mappings(m =>
               m.FluentMappings.AddFromAssemblyOf<City>()
@@ -23,16 +23,58 @@ namespace roadrouting.database {
         }
 
         /// <summary>
-        /// SQLite file at path: ...\dotnet-adapters-shortest-paths-example-project\bin\Debug\roadrouting.db
+        /// SQLite file at path: ...\dotnet-adapters-shortest-paths-example-project\sqlite_directory\roadrouting.db
         /// </summary>
-        private static string DbFile = "roadrouting.db";
+        private static string NameOfSQLiteFileWithoutPath = "roadrouting.sqlite";
+        private static string NameOfSQLiteDirectory = "sqlite_directory";
+        // SQLite commands to look in the database:
+        // cd [ the path to the directory ...\dotnet-adapters-shortest-paths-example-project\sqlite_directory ]
+        // sqlite3 roadrouting.sqlite
+        // .tables (displays the existing tables, should be City and Road)
+        // SELECT * FROM Road;
+        // SELECT * FROM City;
+        // .quit
 
         private static void BuildSchema(Configuration config) {
           //if (File.Exists(DbFile))
           //  File.Delete(DbFile);
-          bool databaseFilexists = File.Exists(DbFile);
+          bool databaseFilexists = File.Exists(GetFullPathToSQLiteFile());
           var schemaExport = new SchemaExport(config);
-          schemaExport.Create(useStdOut: true, execute: !databaseFilexists);
+          //schemaExport.Create(useStdOut: true, execute: !databaseFilexists);
+          schemaExport.Create(useStdOut: true, execute: true);
+        }
+
+        private static string GetFullPathToSQLiteFile() {
+            string fullPathToSQLiteFile = GetFullPath(NameOfSQLiteFileWithoutPath);
+            //Console.WriteLine("GetFullPathToSQLiteFile() : " + fullPathToSQLiteFile);
+            return fullPathToSQLiteFile;
+        }
+        
+        private static string GetFullPath(string fileNameWithoutDirectoryPath) {
+            string basePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var directory = new DirectoryInfo(basePath);
+            directory = TryGetSQLiteSubDirectoryOfProjectRoot(directory);
+            string fullPath = Path.Combine(directory.FullName, fileNameWithoutDirectoryPath);
+            return fullPath;
+        }
+
+        private static DirectoryInfo TryGetSQLiteSubDirectoryOfProjectRoot(DirectoryInfo directory) {
+            //Console.WriteLine("Name of ExecutingAssembly: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+            string projectName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name; // "dotnet-adapters-shortest-paths-example-project"
+            int maxNumberOfBaseDirectoriesToNavigate = 3;  // tyipcally only two: debug and bin (bin/debug)
+            int count = 0;
+            var baseDirectory = directory;
+            while(count < maxNumberOfBaseDirectoriesToNavigate) {
+                baseDirectory = baseDirectory.Parent;
+                if(baseDirectory.Name.Equals(projectName)) {
+                    var sqlite_directory = new DirectoryInfo(Path.Combine(baseDirectory.FullName, NameOfSQLiteDirectory));
+                    if(!sqlite_directory.Exists) {
+                        sqlite_directory.Create();
+                    }
+                    return sqlite_directory;
+                }
+            }
+            return directory;
         }
     }
 }
