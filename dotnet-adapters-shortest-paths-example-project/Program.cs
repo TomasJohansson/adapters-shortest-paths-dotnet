d2718dfb3f6e503f3eec412b47a34a0a1310a601
Tomas Johansson <github-tomas@programmerare.com>
2018-07-14 16:34:50 +0200

    Created and added a new .NET Framework (4.7.2) Console App (an example project) to the Visual Studio 2017 Solution.

---
+++ dotnet-adapters-shortest-paths-example-project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_adapters_shortest_paths_example_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Console App");
+            Console.ReadLine();
+        }
+    }
+}
